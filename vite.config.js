e568ddd0205121006f2f1738b0a37d2d0caad0ea
RmzGerardo <lalitomendez132@gmail.com>
2025-11-19 15:05:44 -0600

    Creacion de rama Gerardo

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'path'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()], 
+  plugins: [react()],
   server: {
     host: true,
-    port: 3000
+    port: 3010,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src')
+      "@": path.resolve(__dirname, "src"),
     },
-  }
-})
+  },
+});
